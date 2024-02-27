6bfcfcf18892f05db26921cd64856bdd69afc527
Alex <goodmorozov@icloud.com>
2024-02-27 23:41:22 +0300

    feat(shared): (routing): added new paths

--- src/shared/routing/index.ts
+++ src/shared/routing/index.ts
@@ -2,4 +2,17 @@ export const paths = {
   home: '/',
   auth: '/auth',
   dashboard: '/dashboard',
+  trade: '/trade',
+  coinCategories: 'coins/categories',
+  coinShowroom: (
+    filter:
+      | 'top-gainers'
+      | 'top losers'
+      | 'recently-added'
+      | 'trending'
+      | 'most-popular'
+  ) => `coins/showroom/${filter}`,
+  nftGuide: 'nft/guide',
+  nftCollections: 'nft/collections',
+  nftWatch: 'nft/whale-watch',
 }
