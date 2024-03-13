8796860c2e1f697cd2369c9cb7b512b5651d2629
Alex <goodmorozov@icloud.com>
2024-03-14 00:31:45 +0300

    perf(features): (small-chart): added spinner

--- src/features/small-chart/index.tsx
+++ src/features/small-chart/index.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { cn } from '@/shared/lib'
 import { getPoints } from './lib'
+import { Spinner } from '@/shared/ui'
 
 interface IChartProps {
   prices?: number[]
@@ -27,22 +28,28 @@ export const SmallChart: React.FC<IChartProps> = ({
     const pointsSrt = getPoints({ arr: prices, current: ref.current as any })
     setPoints(pointsSrt)
   }, [ref, prices])
-
   return (
-    <svg
-      ref={ref}
-      xmlns="http://www.w3.org/2000/svg"
-      version="1.1"
-      className={cn('inline-block w-full h-full', strokeColor, className)}
-      preserveAspectRatio="none"
-    >
-      <polyline
-        points={points}
-        strokeLinecap="round"
-        strokeWidth="1.4"
-        fill="none"
-        vectorEffect="non-scaling-stroke"
-      ></polyline>
-    </svg>
+    <div className="relative">
+      {!points && (
+        <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Spinner size={40} />
+        </span>
+      )}
+      <svg
+        ref={ref}
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        className={cn('inline-block w-full h-full', strokeColor, className)}
+        preserveAspectRatio="none"
+      >
+        <polyline
+          points={points}
+          strokeLinecap="round"
+          strokeWidth="1.4"
+          fill="none"
+          vectorEffect="non-scaling-stroke"
+        ></polyline>
+      </svg>
+    </div>
   )
 }
