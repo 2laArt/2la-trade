933d5c066d0934e4293ce3256f39dab1a6bc2ca5
Alex <goodmorozov@icloud.com>
2024-03-17 00:08:01 +0300

    feat(app): (public/nft/collection): created polygon, solana pages

---
+++ src/app/(public)/nft/collections/polygon/page.tsx
@@ -0,0 +1,51 @@
+import { type NextPage } from 'next'
+import { SectionIntro } from '@/shared/ui/section-intro'
+import React from 'react'
+import { NftTable } from '@/widget/nft-table'
+import { nftServices } from '@/entities/nft/model'
+import { IntroSectionNftCollection } from '@/widget/intro-section-nft-collection'
+
+const NftPolygon: NextPage<{
+  searchParams: {
+    page: string
+    limit: string
+  }
+}> = async ({ searchParams: { page: pageUrl } }) => {
+  const page = Number(pageUrl) || 1
+  const limit = 10
+  const blockchain = 4
+  const symbol = 'ETH'
+  const { data, total } = await nftServices.collections({
+    blockchain,
+    limit,
+    page,
+  })
+
+  return (
+    <div>
+      <div>
+        <SectionIntro title="Polygon NFTs">
+          <p>
+            Polygon Chain NFT Collections shows the name, sales and volume of
+            the top NFTs native to the Polygon blockchain.
+          </p>
+          <p>
+            Discover the top NFT collections on the Polygon blockchain. View the
+            collection value, floor price, 7-day transaction volume, and more.
+          </p>
+        </SectionIntro>
+      </div>
+      <IntroSectionNftCollection blockchain={blockchain} />
+      <NftTable
+        symbol={symbol}
+        isLoading={false}
+        limit={limit}
+        data={data}
+        title="Polygon Blockchain NFT"
+        total={total}
+      />
+    </div>
+  )
+}
+
+export default NftPolygon
---
+++ src/app/(public)/nft/collections/solana/page.tsx
@@ -0,0 +1,51 @@
+import { type NextPage } from 'next'
+import { SectionIntro } from '@/shared/ui/section-intro'
+import React from 'react'
+import { NftTable } from '@/widget/nft-table'
+import { nftServices } from '@/entities/nft/model'
+import { IntroSectionNftCollection } from '@/widget/intro-section-nft-collection'
+
+const NftSolana: NextPage<{
+  searchParams: {
+    page: string
+    limit: string
+  }
+}> = async ({ searchParams: { page: pageUrl } }) => {
+  const page = Number(pageUrl) || 1
+  const limit = 10
+  const blockchain = 2
+  const symbol = 'SOL'
+  const { data, total } = await nftServices.collections({
+    blockchain: 2,
+    limit,
+    page,
+  })
+
+  return (
+    <div>
+      <div>
+        <SectionIntro title="Solana NFTs">
+          <p>
+            Solana NFT Collections shows the name, sales and volume of the top
+            NFTs on the Solana blockchain.
+          </p>
+          <p>
+            Discover the top NFT collections on the Solana blockchain. View the
+            collection value, floor price, 7-day transaction volume, and more.
+          </p>
+        </SectionIntro>
+      </div>
+      <IntroSectionNftCollection blockchain={blockchain} warnMark />
+      <NftTable
+        symbol={symbol}
+        isLoading={false}
+        limit={limit}
+        data={data}
+        title="Solana blockchain NFT"
+        total={total}
+      />
+    </div>
+  )
+}
+
+export default NftSolana
