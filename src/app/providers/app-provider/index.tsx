682595cfc428a4a64d2601c5a8b2ba2652ee4a21
Alex <goodmorozov@icloud.com>
2024-03-27 22:46:57 +0300

    refactor(app): added cart provider | fixed styles

--- src/app/providers/app-provider/index.tsx
+++ src/app/providers/app-provider/index.tsx
@@ -4,10 +4,15 @@ import { queryClient } from '@/shared/api'
 import { ComposeComponents } from '@/shared/lib'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { type FC, type ReactNode } from 'react'
-
+import { SessionProvider } from 'next-auth/react'
+import { UserCartProvider } from '@/entities/cart/context'
 export const AppProvider: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <ComposeComponents>
+      <SessionProvider>
+        <></>
+      </SessionProvider>
+      <UserCartProvider />
       <ThemeProvider />
       <QueryClientProvider client={queryClient} />
       {children}
