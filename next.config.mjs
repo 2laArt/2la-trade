000bd30f8c543071eeda84739aa280215970119a
Alex <goodmorozov@icloud.com>
2024-03-27 22:23:14 +0300

    build(custom): new dependencies have been added | setup next-config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'static.crypto.com',
+        hostname: '**',
         pathname: '**',
       },
     ],
