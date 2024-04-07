845dde4b26bae9478ba06f407bf60cd0c41943a1
Alex <goodmorozov@icloud.com>
2024-04-07 20:35:33 +0300

    feat(app): (public/trade): the stub page

---
+++ src/app/(public)/trade/page.tsx
@@ -0,0 +1,10 @@
+import { type NextPage } from 'next'
+import React from 'react'
+const Trade: NextPage = async () => {
+  return (
+    <div className="text-center text-3xl font-semibold">
+      Sorry, Return later
+    </div>
+  )
+}
+export default Trade
