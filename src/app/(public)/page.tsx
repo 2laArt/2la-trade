48fce4962f1c3ccdbdaddba6fa589a439ff84eab
Alex <goodmorozov@icloud.com>
2024-04-22 14:37:37 +0300

    refactor(app): (public): fetch data

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,36 +1,40 @@
-/* eslint-disable @next/next/no-img-element */
 'use client'
 import { CoinTable } from '@/widget/coin-table'
 import { TopMoversSlider } from '@/features/top-movers-slider'
-import { useQuery } from '@tanstack/react-query'
 import { NextPage } from 'next'
 import React from 'react'
-import { coinsServices } from '@/entities/coins-list'
+import { useQueryCoinsPage, useQueryTopMovers } from '@/entities/coins-list'
 
 const Home: NextPage<{
   searchParams: {
     page: string
     limit: string
   }
-}> = ({ searchParams: { limit, page } }) => {
-  const { data: coins, isLoading } = useQuery({
-    queryKey: ['topMovers'],
-    queryFn: () => coinsServices.topMovers({ depth: 10 }),
+}> = ({ searchParams: { page } }) => {
+  const curPage = Number(page || '1')
+  const { data: movers, isLoading: isMovers } = useQueryTopMovers({ depth: 10 })
+  const { data: coins, isLoading } = useQueryCoinsPage({
+    limit: 50,
+    page: curPage,
   })
-
   return (
     <div>
       <section className="my-5">
         <h3 className="text-lg font-semibold">Top Movers</h3>
-        {!!coins && (
+        {
           <TopMoversSlider
-            isLoading={isLoading}
-            coinList={coins}
+            isLoading={isMovers}
+            coinList={movers}
             currency="USD"
           />
-        )}
+        }
       </section>
-      <CoinTable isLoading={isLoading} limit={50} data={coins} />
+      <CoinTable
+        isLoading={isLoading}
+        limit={50}
+        data={coins?.data}
+        total={coins?.total}
+      />
     </div>
   )
 }
--- src/app/(public)/trade/page.tsx
+++ src/app/(public)/trade/page.tsx
@@ -1,9 +1,17 @@
 import { type NextPage } from 'next'
+import Image from 'next/image'
 import React from 'react'
 const Trade: NextPage = async () => {
   return (
     <div className="text-center text-3xl font-semibold">
-      Sorry, Return later
+      <div className="mb-8">The page is under development, come back later</div>
+      <Image
+        src={'/images/feature.png'}
+        alt="feature"
+        width={500}
+        height={250}
+        className="mx-auto"
+      />
     </div>
   )
 }
