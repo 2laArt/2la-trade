170d956cdd5dc8455f3d047265079bb7373ad508
Alex <goodmorozov@icloud.com>
2024-04-22 18:38:38 +0300

    setup prisma

--- src/shared/lib/db/index.ts
+++ src/shared/lib/db/index.ts
@@ -1,10 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
+import { DefaultArgs } from '@prisma/client/runtime/library'
 
+type PrismaType = PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>
 declare global {
-  var prisma: PrismaClient | undefined
+  namespace globalThis {
+    var prisma: PrismaType
+  }
 }
 
 const prisma = global.prisma || new PrismaClient({ log: ['info'] })
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (process.env.NEXT_PUBLIC_NODE_ENV !== 'production') global.prisma = prisma
 
 export { prisma }
